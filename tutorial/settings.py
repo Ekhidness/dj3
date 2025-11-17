e2b690bd4bd79a7e6e7e01a5081e16c3805ba7a3
Ekhidna <ekhidna@disroot.org>
2025-11-17 20:53:50 +0700

    Preparing for a push to PythonAnywhere

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2=#5e+%^k8auj5zjo=zgqif=x#@#begri2y@m-&2hu=0k*fzf6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ekhidna.pythonanywhere.com']
 
 
 # Application definition
