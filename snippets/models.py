db0395c1aa7bdcacb2f3dabd75de8bce5449df34
ekhidna <ekhidna@disroot.org>
2025-11-18 12:20:33 +0700

    merge conflict fix

--- snippets/models.py
+++ snippets/models.py
@@ -20,11 +20,15 @@ class Snippet(models.Model):
     highlighted = models.TextField()
 
     def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
         lexer = get_lexer_by_name(self.language)
         linenos = 'table' if self.linenos else False
         options = {'title': self.title} if self.title else {}
         formatter = HtmlFormatter(style=self.style, linenos=linenos,
-                                full=True, **options)
+                                  full=True, **options)
         self.highlighted = highlight(self.code, lexer, formatter)
         super(Snippet, self).save(*args, **kwargs)
 
